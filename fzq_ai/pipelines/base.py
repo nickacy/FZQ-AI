38813db226019f131a6bed7c16c2b925c261303e
你的用户名 <>
2026-06-18 09:23:58 +1000

    git pull

---
+++ fzq_ai/pipelines/base.py
@@ -0,0 +1,12 @@
+from typing import Generic, TypeVar, Optional
+from fzq_ai.schemas.base import PipelineOutputSchema
+
+T = TypeVar("T", bound=PipelineOutputSchema)
+
+
+class BasePipeline(Generic[T]):
+    """所有 Pipeline 的泛型基类"""
+
+    async def run_async(self, *args, **kwargs) -> T:
+        """子类必须实现"""
+        raise NotImplementedError
